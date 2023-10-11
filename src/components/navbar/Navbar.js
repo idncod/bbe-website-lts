e565defa90b2200c7de2fd25b240bd2010020ede
idncod <idncod@protonmail.com>
2023-10-11 21:56:01 +0100

    home link added

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { FaBars, FaTimes, FaCottonBureau } from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import './NavbarStyles.css'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => setNav(!nav)
+
+
+    return (
+        <div name='top' className='navbar'>
+            <div className="container">
+                    <Link to="/" className="logo" >
+                    <FaCottonBureau className='icon' />
+                    <h3>Beauty By Evans</h3>
+                    </Link>
+                    
+                <ul className={nav ? 'nav-menu active' : 'nav-menu'}>
+                    <li><Link to='/'>About</Link></li>
+                    <li><Link to='/services'>Services</Link> </li>
+                    <li><Link to='/contact'>Contact</Link></li>
+                    <button>Book Now</button>
+                </ul>
+                <div className="hamburger" onClick={handleNav}>
+                    {!nav ? (<FaBars className='icon' />) : (<FaTimes className='icon' />)}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
